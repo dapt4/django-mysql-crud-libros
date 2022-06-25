de2edc495f8582c560f57c6d97f74a13d91cee32
Diego Perozo <diegodapt18@gmail.com>
2022-06-25 13:03:14 -0400

    editing the settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4cj5&a91&r$1^^0l_role@j-jz4cez73$@0jkw+dxa3b&&i-)%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
